95204cc80a6270b270026b61dde7fec59119af94
sj <asj12896@gmail.com>
2024-10-21 18:55:04 +0900

    Initial commit

---
+++ src/test/java/studio/_0/bbang/BbangApplicationTests.java
@@ -0,0 +1,13 @@
+package studio._0.bbang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
