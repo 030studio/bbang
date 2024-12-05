9984953a10ad38a82ac9f28f9d0e250cc8803ad3
minkyeong_kim <dufma9385@gmail.com>
2024-12-06 00:49:20 +0900

    test 코드 뼈대 생성...

---
+++ src/main/java/studio/zero/bbang/service/MapService.java
@@ -0,0 +1,7 @@
+package studio.zero.bbang.service;
+
+public class MapService {
+    public void getStoreList() {
+
+    }
+}
---
+++ src/test/java/studio/zero/bbang/service/MapServiceTest.java
@@ -0,0 +1,20 @@
+package studio.zero.bbang.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MapServiceTest {
+
+    @Test
+    void getStoreList() {
+        // given
+        // 현재 위치 받아서 전달
+
+        // when
+        // 현재 위치 정보 미흡으로 실패
+
+        // then
+        // 리스트 잘 불러왔는지 확인
+    }
+}
